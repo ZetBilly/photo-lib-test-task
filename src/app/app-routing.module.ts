2d023d39839e6bac45a1afd09526cbfb88635f42
Alexander Plysheuski <Alexander_Plysheuski@epam.com>
2023-04-20 19:16:59 +0200

    bugfix: incorrect route highlighting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { FavoritesComponent } from './photos/favorites/favorites.component';
 import { DetailViewComponent } from './photos/detail-view/detail-view.component';
 
 const routes: Routes = [
+  { path: '', component: PhotoFeedComponent },
   { path: 'favorites', component: FavoritesComponent },
   { path: 'photos/:id', component: DetailViewComponent },
-  { path: '', component: PhotoFeedComponent },
   { path: '**', redirectTo:  '', pathMatch: 'full' },
 ];
 
